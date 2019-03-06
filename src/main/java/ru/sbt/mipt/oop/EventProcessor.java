cb3b05afc53f134614521daf109b4684b37558bb
Aleksei Karpov <ahbpp@hotmail.com>
2019-03-06 12:06:27 +0300

    "refact application"

---
+++ src/main/java/ru/sbt/mipt/oop/EventProcessor.java
@@ -0,0 +1,6 @@
+package ru.sbt.mipt.oop;
+
+public class EventProcessor {
+    public void runEvent(SensorEvent event, SmartHome smartHome) {
+    }
+}
---
+++ src/main/java/ru/sbt/mipt/oop/LightsEventProcessor.java
@@ -0,0 +1,26 @@
+package ru.sbt.mipt.oop;
+
+import static ru.sbt.mipt.oop.SensorEventType.LIGHT_OFF;
+import static ru.sbt.mipt.oop.SensorEventType.LIGHT_ON;
+
+public class LightsEventProcessor extends EventProcessor {
+    @Override
+    public void runEvent(SensorEvent event, SmartHome smartHome) {
+        if (event.getType() == LIGHT_ON || event.getType() == LIGHT_OFF) {
+            // событие от источника света
+            for (Room room : smartHome.getRooms()) {
+                for (Light light : room.getLights()) {
+                    if (light.getId().equals(event.getObjectId())) {
+                        if (event.getType() == LIGHT_ON) {
+                            light.setOn(true);
+                            System.out.println("Light " + light.getId() + " in room " + room.getName() + " was turned on.");
+                        } else {
+                            light.setOn(false);
+                            System.out.println("Light " + light.getId() + " in room " + room.getName() + " was turned off.");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
